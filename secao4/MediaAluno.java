8201fd78f1db09a7e8b41d8466376099a3768ff4
Douglas Willian <118227603+Doug1Gogax@users.noreply.github.com>
2026-08-27 19:28:30 +0000

    adicionando secao4

--- secao4/MediaAluno.java
+++ secao4/MediaAluno.java
@@ -3,7 +3,39 @@ package secao4;
 import java.util.Scanner;
 
 public class MediaAluno {
-    
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite o nome do Aluno:");
+    String nome = scanner.nextLine();
+        
+    System.out.println("Digite a primeira nota:");
+    double nota1 = scanner.nextDouble();
+
+    System.out.println("Digite a segunda nota:");
+    double nota2 = scanner.nextDouble();
+
+    System.out.println("Digite a terceira nota:");
+    double nota3= scanner.nextDouble();
+
+    double media = (nota1 + nota2 + nota3) / 3;
+
+    System.out.println("O nome do alune é:" + nome + ". E sua medio foi:" + media);
+
+    if(media >= 7){
+        System.out.println("Aluno aprovado");
+
+    }else{
+        System.out.println("Aluno reprovado");
+    }
+
+    scanner.close();
+        
+    }
+
+   
+
 
 
     
